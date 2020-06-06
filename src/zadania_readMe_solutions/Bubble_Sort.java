442bb40b9370b8de4477fd81c8595908b6238ea3
Paulina <bura.bury@gmail.com>
2020-06-06 13:17:57 +0200

    lalalala

--- src/zadania_readMe_solutions/Bubble_Sort.java
+++ src/zadania_readMe_solutions/Bubble_Sort.java
@@ -1,11 +1,29 @@
 package zadania_readMe_solutions;
 
+import java.util.Arrays;
+
 public class Bubble_Sort {
     public static void main(String[] args) {
 
-
-
-
+        int[] tab = {2, 0, 4, 10, 3, 1, 5};
+        swap(tab);
 
     }
+
+    private static void swap(int[] arr) {
+        int value;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    value = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = value;
+                }
+            }
+        }
+        System.out.println("Posortowana tablica: ");
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
