6786ac89afd93f7a9c83a74b55783dd280ffb1ad
Paulina <bura.bury@gmail.com>
2020-06-06 12:36:37 +0200

    lalala

--- src/zadania_readMe_solutions/ArraysPointer2.java
+++ src/zadania_readMe_solutions/ArraysPointer2.java
@@ -1,24 +1,26 @@
 package zadania_readMe_solutions;
+//sumowanie kolejnych par cyfr w tablicy
 
 public class ArraysPointer2 {
     public static void main(String[] args) {
-        int array[] = {1, 2, 3, 4, 5, 6, 7};
-        funnyArray(array);
-
+        int array[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        printPairSum(array);
     }
 
-    private static void funnyArray(int[] tab) {
-        int start = 0;
-        int second = 1;
-        int sum;
+    private static void printPairSum(int[] arr) {
+        int left = 0;
+        int right = 1;
+        if (arr.length % 2 == 0 && arr.length > 1) {
 
-        while (second <= (tab.length - 1)) {
-            sum = tab[start] + tab[second];
-            start++;
-            second++;
-            System.out.println(sum);
+            while (right < arr.length) {
+                System.out.println(arr[left] + arr[right]);
+
+                left += 2;
+                right += 2;
+            }
+
+        } else {
+            System.out.println("Nieprawidłowa tablica");
         }
-
-
     }
 }
