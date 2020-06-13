c7e3a008068e842ca6c85c30a35b58d9fd2130eb
Paulina <bura.bury@gmail.com>
2020-06-13 17:02:49 +0200

    13.06

---
+++ src/solutions_ReadMe_Exercises/LiczbyParzysteW_Przedziale.java
@@ -0,0 +1,20 @@
+package solutions_ReadMe_Exercises;
+
+import java.util.Scanner;
+
+public class LiczbyParzysteW_Przedziale {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            for (int i = a; i <= b; i++) {
+                if (i % 2 == 0) {
+                    System.out.printf("%d ", i);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Błąd, to nie jest poprawna liczba.");
+        }
+    }
+}
